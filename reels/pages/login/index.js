da16ad87b0a4fec0ecff3789624bc32b298c3adf
Rinaldo23 <[rinaldobadigar98@gmail.com]>
2022-11-25 22:04:31 +0530

    Added Carousel functionality in login page

--- reels/pages/login/index.js
+++ reels/pages/login/index.js
@@ -4,13 +4,28 @@ import Button from '@mui/material/Button';
 import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
 import Insta from '../../images/Insta.png'
+import bg1 from '../../images/carousel_slide_1.png'
+import bg2 from '../../images/carousel_slide_2.png'
+import bg3 from '../../images/carousel_slide_3.png'
 
 function login() {
   return (
     <div className='login-main-page'>
-      <div className='carousel-background'>
-        <Carousel>
-        </Carousel>
+      <div className='car-bg'>
+        <div className='car'>
+          <Carousel showIndicators={false}
+            showArrows={false}
+            autoPlay={true}
+            infiniteLoop={true}
+            showThumbs={false}
+            showStatus={false}
+            interval={2000}
+            transitionTime={1000}>
+            <Image src={bg1} style={{ height: '31rem' }} />
+            <Image src={bg2} style={{ height: '31rem' }} />
+            <Image src={bg3} style={{ height: '31rem' }} />
+          </Carousel>
+        </div>
       </div>
       <div className='login'>
         <div className='login-container'>
